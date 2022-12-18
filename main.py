7cbf79813f827f1eeaba4fe6d8d191318a23300f
Денис Толкачев <opexy@e1.ru>
2022-12-18 22:31:24 +0500

    With CODEstring

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Attack the opponent."""
     if char_class == 'warrior':
         return (f'{char_name} '
                 'нанёс урон противнику равный'
@@ -18,6 +21,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Defend from the blow."""
     if char_class == 'warrior':
         return (f'{char_name}'
                 ' блокировал {10 + randint(5, 10)} урона')
@@ -31,6 +35,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Use special skill."""
     if char_class == 'warrior':
         return (f'{char_name}'
                 ' применил специальное умение «Выносливость {80 + 25}»')
@@ -44,6 +49,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Do training now."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -68,6 +74,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Choose the character."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -89,7 +96,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
